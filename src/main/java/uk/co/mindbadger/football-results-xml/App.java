62d0e2cd5a33f440f76b1dc243ecf086230302ae
Mark on LAPPY <growler1970@gmail.com>
2013-01-19 15:36:19 +0000

    Initial checkin with simple maven build

---
+++ src/main/java/uk/co/mindbadger/football-results-xml/App.java
@@ -0,0 +1,13 @@
+package uk.co.mindbadger.football-results-xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
