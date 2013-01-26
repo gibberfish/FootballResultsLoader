2c03a300e14f5efddcb1197a8fd32b64e4621bff
Mark Fowler <growler1970@gmail.com>
2013-01-26 11:03:00 +0000

    Fixed broken test

--- src/main/java/uk/co/mindbadger/footballresults/reader/xml/SoccerbaseXMLReader.java
+++ src/main/java/uk/co/mindbadger/footballresults/reader/xml/SoccerbaseXMLReader.java
@@ -16,101 +16,102 @@ import uk.co.mindbadger.footballresults.reader.ParsedFixture;
 import uk.co.mindbadger.xml.XMLFileReader;
 
 public class SoccerbaseXMLReader implements FootballResultsReader {
-	private XMLFileReader xmlFileReader;
-	private String rootDirectory;
+    private XMLFileReader xmlFileReader;
+    private String rootDirectory;
 
-	@Override
-	public List<ParsedFixture> readFixturesForSeason(int seasonNumber) {
-		List<ParsedFixture> fixtures = new ArrayList<ParsedFixture>();
+    @Override
+    public List<ParsedFixture> readFixturesForSeason(int seasonNumber) {
+	List<ParsedFixture> fixtures = new ArrayList<ParsedFixture>();
 
-		List<String> fullyQualifiedSeasonFileNames = xmlFileReader.getFilesInDirectory(rootDirectory + "\\" + seasonNumber);
+	List<String> fullyQualifiedSeasonFileNames = xmlFileReader.getFilesInDirectory(rootDirectory + "\\" + seasonNumber);
 
-		for (String fileName : fullyQualifiedSeasonFileNames) {
-			try {
-				Document doc = xmlFileReader.readXMLFile(fileName);
+	for (String fileName : fullyQualifiedSeasonFileNames) {
+	    try {
+		Document doc = xmlFileReader.readXMLFile(fileName);
 
-				Element rootElement = doc.getDocumentElement();
+		Element rootElement = doc.getDocumentElement();
 
-				Calendar fixtureDate = convertDateStringToCalendar(rootElement.getAttribute("date"));
+		Calendar fixtureDate = convertDateStringToCalendar(rootElement.getAttribute("date"));
 
-				NodeList competitions = rootElement.getElementsByTagName("Competition");
+		NodeList competitions = rootElement.getElementsByTagName("Competition");
 
-				for (int i = 0; i < competitions.getLength(); i++) {
-					Element competition = (Element) competitions.item(i);
+		for (int i = 0; i < competitions.getLength(); i++) {
+		    Element competition = (Element) competitions.item(i);
 
-					Integer seasonId = Integer.parseInt(competition.getAttribute("seasonId")) + 1870;
-					if (seasonId != seasonNumber) {
-						throw new FootballResultsReaderException("Your xml file contains a season that is not in the correct folder");
-					}
+		    Integer seasonId = Integer.parseInt(competition.getAttribute("seasonId")) + 1870;
+		    if (seasonId != seasonNumber) {
+			throw new FootballResultsReaderException("Your xml file contains a season that is not in the correct folder");
+		    }
 
-					Integer competitionId = Integer.parseInt(competition.getAttribute("competitionId"));
-					String competitionName = competition.getAttribute("competitionName");
+		    Integer competitionId = Integer.parseInt(competition.getAttribute("competitionId"));
+		    String competitionName = competition.getAttribute("competitionName");
 
-					NodeList games = competition.getElementsByTagName("Game");
-					for (int j = 0; j < games.getLength(); j++) {
-						Element game = (Element) games.item(j);
+		    NodeList games = competition.getElementsByTagName("Game");
+		    for (int j = 0; j < games.getLength(); j++) {
+			Element game = (Element) games.item(j);
 
-						Integer fixtureId = Integer.parseInt(game.getAttribute("gameId"));
-						Integer homeTeamId = Integer.parseInt(game.getAttribute("homeTeamId"));
-						Integer awayTeamId = Integer.parseInt(game.getAttribute("awayTeamId"));
-						String homeTeamName = game.getAttribute("homeTeamName");
-						String awayTeamName = game.getAttribute("awayTeamName");
+			Integer fixtureId = Integer.parseInt(game.getAttribute("gameId"));
+			Integer homeTeamId = Integer.parseInt(game.getAttribute("homeTeamId"));
+			Integer awayTeamId = Integer.parseInt(game.getAttribute("awayTeamId"));
+			String homeTeamName = game.getAttribute("homeTeamName");
+			String awayTeamName = game.getAttribute("awayTeamName");
 
-						ParsedFixture fixture = new ParsedFixture();
-						fixture.setFixtureId(fixtureId);
-						fixture.setSeasonId(seasonId);
-						fixture.setDivisionId(competitionId);
-						fixture.setDivisionName(competitionName);
-						fixture.setFixtureDate(fixtureDate);
-						fixture.setHomeTeamId(homeTeamId);
-						fixture.setHomeTeamName(homeTeamName);
-						fixture.setAwayTeamId(awayTeamId);
-						fixture.setAwayTeamName(awayTeamName);
-						
-						fixtures.add(fixture);
+			ParsedFixture fixture = new ParsedFixture();
+			fixture.setFixtureId(fixtureId);
+			fixture.setSeasonId(seasonId);
+			fixture.setDivisionId(competitionId);
+			fixture.setDivisionName(competitionName);
+			fixture.setFixtureDate(fixtureDate);
+			fixture.setHomeTeamId(homeTeamId);
+			fixture.setHomeTeamName(homeTeamName);
+			fixture.setAwayTeamId(awayTeamId);
+			fixture.setAwayTeamName(awayTeamName);
 
-						NodeList scores = game.getElementsByTagName("Score");
-						Element score = (Element) scores.item(0);
-						if (score != null) {
-							fixture.setHomeGoals(Integer.parseInt(score.getAttribute("homeGoals")));
-							fixture.setAwayGoals(Integer.parseInt(score.getAttribute("awayGoals")));
-						}
-					}
-				}
-			} catch (Exception e) {
-				throw new FootballResultsReaderException(e);
+			fixtures.add(fixture);
+
+			NodeList scores = game.getElementsByTagName("Score");
+			Element score = (Element) scores.item(0);
+			if (score != null) {
+			    fixture.setHomeGoals(Integer.parseInt(score.getAttribute("homeGoals")));
+			    fixture.setAwayGoals(Integer.parseInt(score.getAttribute("awayGoals")));
 			}
+		    }
 		}
-
-		return fixtures;
+	    } catch (Exception e) {
+		throw new FootballResultsReaderException(e);
+	    }
 	}
 
-	private Calendar convertDateStringToCalendar(String dateString) {
-		Calendar cal = Calendar.getInstance();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		try {
-			cal.setTime(sdf.parse(dateString));
-			return cal;
-		} catch (ParseException e) {
-			// TODO Need to add a test to deal with being unable to parse the date
-			throw new FootballResultsReaderException(e);
-		}
-	}
+	return fixtures;
+    }
 
-	public XMLFileReader getXmlFileReader() {
-		return xmlFileReader;
+    private Calendar convertDateStringToCalendar(String dateString) {
+	Calendar cal = Calendar.getInstance();
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	try {
+	    cal.setTime(sdf.parse(dateString));
+	    return cal;
+	} catch (ParseException e) {
+	    // TODO Need to add a test to deal with being unable to parse the
+	    // date
+	    throw new FootballResultsReaderException(e);
 	}
+    }
 
-	public void setXmlFileReader(XMLFileReader xmlFileReader) {
-		this.xmlFileReader = xmlFileReader;
-	}
+    public XMLFileReader getXmlFileReader() {
+	return xmlFileReader;
+    }
 
-	public String getRootDirectory() {
-		return rootDirectory;
-	}
+    public void setXmlFileReader(XMLFileReader xmlFileReader) {
+	this.xmlFileReader = xmlFileReader;
+    }
 
-	public void setRootDirectory(String rootDirectory) {
-		this.rootDirectory = rootDirectory;
-	}
+    public String getRootDirectory() {
+	return rootDirectory;
+    }
+
+    public void setRootDirectory(String rootDirectory) {
+	this.rootDirectory = rootDirectory;
+    }
 
 }
