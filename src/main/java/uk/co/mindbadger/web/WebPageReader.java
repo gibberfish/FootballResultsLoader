039ed5e078a72bad0ee4ef249e67310295cf5075
Mark Fowler <growler1970@gmail.com>
2013-01-31 20:26:05 +0000

    Added basic vanilla web page reader

---
+++ src/main/java/uk/co/mindbadger/web/WebPageReader.java
@@ -0,0 +1,46 @@
+package uk.co.mindbadger.web;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class WebPageReader {
+	public ArrayList<String> readWebPage(String pURL) throws FileNotFoundException, IOException {
+		ArrayList<String> results = new ArrayList <String>();
+		BufferedReader in = null;
+		InputStreamReader webReader = null;
+
+		try {
+			URL url = new URL(pURL);
+			webReader = new InputStreamReader(url.openStream());
+			in = new BufferedReader(webReader);
+
+			String line;
+			while ((line = in.readLine()) != null) {
+				results.add(line);
+			}
+		} finally {
+			// Force the close of the file...
+			if (webReader != null)
+				try {
+					webReader.close();
+				} catch (IOException e) {
+					;
+				}
+		}
+
+		return results;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		WebPageReader reader = new WebPageReader();
+		ArrayList<String> output = reader.readWebPage("http://www.soccerbase.com/matches/results.sd?date=2012-11-28");
+		for (String line : output) {
+			System.out.println(line);
+		}
+	}
+
+}
