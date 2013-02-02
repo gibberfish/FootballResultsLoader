5e620ff29be691fc12938d7eedb32052e3df57dc
Mark Fowler <growler1970@gmail.com>
2013-02-02 16:28:53 +0000

    Use the equality of the ParsedFixture in combination with a Map to ensure the parsedFixtures are not duplicated

--- src/main/java/uk/co/mindbadger/footballresults/reader/web/SoccerbaseWebPageReader.java
+++ src/main/java/uk/co/mindbadger/footballresults/reader/web/SoccerbaseWebPageReader.java
@@ -1,7 +1,9 @@
 package uk.co.mindbadger.footballresults.reader.web;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import uk.co.mindbadger.footballresults.reader.FootballResultsReader;
 import uk.co.mindbadger.footballresults.reader.ParsedFixture;
@@ -14,20 +16,24 @@ public class SoccerbaseWebPageReader implements FootballResultsReader {
 	@Override
 	public List<ParsedFixture> readFixturesForSeason(int season) {
 		
-		List<ParsedFixture> fixturesForSeason = new ArrayList<ParsedFixture> ();
+		Map <ParsedFixture, ParsedFixture> fixturesForSeason = new HashMap<ParsedFixture, ParsedFixture> ();
 		
 		String boxingDay = season + "-12-26";
 		List<ParsedFixture> boxingDayFixtures = datePageParser.parseFixturesForDate(boxingDay);
 		
 		for (ParsedFixture fixture : boxingDayFixtures) {
 			List<ParsedFixture> homeTeamFixtures = teamPageParser.parseFixturesForTeam(season, fixture.getHomeTeamId());
-			List<ParsedFixture> awayTeamFixtures = teamPageParser.parseFixturesForTeam(season, fixture.getAwayTeamId());
+			for (ParsedFixture homeTeamfixture : homeTeamFixtures) {
+				fixturesForSeason.put(homeTeamfixture, homeTeamfixture);
+			}
 			
-			fixturesForSeason.addAll(homeTeamFixtures);
-			fixturesForSeason.addAll(awayTeamFixtures);
+			List<ParsedFixture> awayTeamFixtures = teamPageParser.parseFixturesForTeam(season, fixture.getAwayTeamId());
+			for (ParsedFixture awayTeamfixture : awayTeamFixtures) {
+				fixturesForSeason.put(awayTeamfixture, awayTeamfixture);
+			}
 		}
 		
-		return fixturesForSeason;
+		return new ArrayList<ParsedFixture> (fixturesForSeason.values());
 	}
 
 	public void setDialect(String dialect) {
