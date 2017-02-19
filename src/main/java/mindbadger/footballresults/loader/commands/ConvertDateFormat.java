bdea2fe312c5576e0a7665e5f4dd1c87de951d21
Mark Fowler <themindbadger@gmail.com>
2017-02-19 16:06:02 +0000

    Removed dao references from the Convert date format command

--- src/main/java/mindbadger/footballresults/loader/commands/ConvertDateFormat.java
+++ src/main/java/mindbadger/footballresults/loader/commands/ConvertDateFormat.java
@@ -1,30 +1,21 @@
 package mindbadger.footballresults.loader.commands;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import mindbadger.footballresultsanalyser.dao.FootballResultsAnalyserDAO;
 import mindbadger.footballresultsanalyser.domain.Fixture;
+import mindbadger.footballresultsanalyser.repository.FixtureRepository;
 
 @Component
 public class ConvertDateFormat implements Command {
 
 	@Autowired
-	private FootballResultsAnalyserDAO dao;
+	private FixtureRepository fixtureRepository;
 	
 	@Override
 	public void run(String[] args) throws Exception {
-		dao.startSession();
-		
-		List<Fixture> fixtures = dao.getFixtures();
-		for (Fixture fixture : fixtures) {
-			
-			Fixture fixtureAdded = dao.addFixture(fixture.getSeason(), fixture.getFixtureDate(), fixture.getDivision(),
-					fixture.getHomeTeam(), fixture.getAwayTeam(), fixture.getHomeGoals(), fixture.getAwayGoals());
+		for (Fixture fixture : fixtureRepository.findAll()) {
+			fixtureRepository.save(fixture);
 		}
-		
-		dao.closeSession();
 	}
 }
