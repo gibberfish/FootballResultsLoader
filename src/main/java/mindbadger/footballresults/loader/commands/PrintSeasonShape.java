fe969e274941b83f8c7f4089085453d3671be45c
Mark Fowler <themindbadger@gmail.com>
2017-02-19 19:10:43 +0000

    Removed dao references from the print season shape command

--- src/main/java/mindbadger/footballresults/loader/commands/PrintSeasonShape.java
+++ src/main/java/mindbadger/footballresults/loader/commands/PrintSeasonShape.java
@@ -1,50 +1,47 @@
 package mindbadger.footballresults.loader.commands;
 
-import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import mindbadger.footballresultsanalyser.dao.FootballResultsAnalyserDAO;
 import mindbadger.footballresultsanalyser.domain.Division;
 import mindbadger.footballresultsanalyser.domain.Season;
 import mindbadger.footballresultsanalyser.domain.SeasonDivision;
 import mindbadger.footballresultsanalyser.domain.SeasonDivisionTeam;
 import mindbadger.footballresultsanalyser.domain.Team;
+import mindbadger.footballresultsanalyser.repository.SeasonRepository;
 
 @Component
 public class PrintSeasonShape implements Command {
+
 	@Autowired
-	private FootballResultsAnalyserDAO dao;
+	private SeasonRepository seasonRepository;
 
 	@Override
 	public void run(String[] args) throws Exception {
-		dao.startSession();
-
-		List<Season> seasons = dao.getSeasons();
+		Iterable<Season> seasons = seasonRepository.findAll();
 		
 		for (Season season : seasons) {
 			System.out.println("Season: " + season.getSeasonNumber());
 			
-			List<SeasonDivision> seasonDivisions = dao.getDivisionsForSeason(season);
+			Set<SeasonDivision> seasonDivisions = season.getSeasonDivisions();
 			
 			System.out.println("(" + seasonDivisions.size() + " divisions:");
 			
 			for (SeasonDivision seasonDivision : seasonDivisions) {
-				Division division = dao.getDivision(seasonDivision.getDivision().getDivisionId());
+				Division division = seasonDivision.getDivision();
 				System.out.println("  Division: " + division.getDivisionName());
 				
-				List<SeasonDivisionTeam> seasonDivisionTeams = dao.getTeamsForDivisionInSeason(seasonDivision);
+				Set<SeasonDivisionTeam> seasonDivisionTeams = seasonDivision.getSeasonDivisionTeams();
 				
 				System.out.println("  (" + seasonDivisionTeams.size() + " teams:");
 				
 				for (SeasonDivisionTeam seasonDivisionTeam : seasonDivisionTeams) {
-					Team team = dao.getTeam(seasonDivisionTeam.getTeam().getTeamId());
+					Team team = seasonDivisionTeam.getTeam();
 					System.out.println("    Team: " + team.getTeamName());
 				}
 			}
 		}
-		
-		dao.closeSession();		
 	}
 }
