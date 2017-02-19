5d6a8327bd540d7167c428cd1721d9ba72124de1
Mark Fowler <themindbadger@gmail.com>
2017-02-19 16:33:22 +0000

    Removed dao references from the create season division teams for existing fixtures command

--- src/main/java/mindbadger/footballresults/loader/commands/CreateSeasonDivisionTeamsForExistingFixtures.java
+++ src/main/java/mindbadger/footballresults/loader/commands/CreateSeasonDivisionTeamsForExistingFixtures.java
@@ -1,32 +1,52 @@
 package mindbadger.footballresults.loader.commands;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import mindbadger.footballresultsanalyser.dao.FootballResultsAnalyserDAO;
+import mindbadger.footballresultsanalyser.domain.DomainObjectFactory;
 import mindbadger.footballresultsanalyser.domain.Fixture;
+import mindbadger.footballresultsanalyser.domain.Season;
 import mindbadger.footballresultsanalyser.domain.SeasonDivision;
+import mindbadger.footballresultsanalyser.domain.SeasonDivisionTeam;
+import mindbadger.footballresultsanalyser.repository.FixtureRepository;
+import mindbadger.footballresultsanalyser.repository.SeasonRepository;
 
 @Component
 public class CreateSeasonDivisionTeamsForExistingFixtures implements Command {
 
 	@Autowired
-	private FootballResultsAnalyserDAO dao;
+	private FixtureRepository fixtureRepository;
 	
+	@Autowired
+	private SeasonRepository seasonRepository;
+	
+	@Autowired
+	private DomainObjectFactory domainObjectFactory;
+
 	@Override
 	public void run(String[] args) throws Exception {
-		dao.startSession();
-		
-		List<Fixture> fixtures = dao.getFixtures();
-		
-		for (Fixture fixture : fixtures) {
-			SeasonDivision seasonDivision = dao.addSeasonDivision(fixture.getSeason(), fixture.getDivision(), 0);
-			dao.addSeasonDivisionTeam(seasonDivision, fixture.getHomeTeam());
-			dao.addSeasonDivisionTeam(seasonDivision, fixture.getAwayTeam());
+		for (Fixture fixture : fixtureRepository.findAll()) {
+			Season season = seasonRepository.findMatching(fixture.getSeason());
+
+			SeasonDivision seasonDivision = seasonRepository.getSeasonDivision(fixture.getSeason(), fixture.getDivision());
+			if (seasonDivision == null) {
+				seasonDivision = domainObjectFactory.createSeasonDivision(season, fixture.getDivision(), 0);
+				season.getSeasonDivisions().add(seasonDivision);
+			}
+			
+			SeasonDivisionTeam seasonDivisionTeam = seasonRepository.getSeasonDivisionTeam(seasonDivision, fixture.getHomeTeam());
+			if (seasonDivisionTeam == null) {
+				seasonDivisionTeam = domainObjectFactory.createSeasonDivisionTeam(seasonDivision, fixture.getHomeTeam());
+				seasonDivision.getSeasonDivisionTeams().add(seasonDivisionTeam);
+			}
+
+			seasonDivisionTeam = seasonRepository.getSeasonDivisionTeam(seasonDivision, fixture.getAwayTeam());
+			if (seasonDivisionTeam == null) {
+				seasonDivisionTeam = domainObjectFactory.createSeasonDivisionTeam(seasonDivision, fixture.getAwayTeam());
+				seasonDivision.getSeasonDivisionTeams().add(seasonDivisionTeam);
+			}
+
+			seasonRepository.save(season);
 		}
-		
-		dao.closeSession();
 	}
 }
